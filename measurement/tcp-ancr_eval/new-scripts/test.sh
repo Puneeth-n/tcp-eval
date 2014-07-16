bcb5e875d882b2ed0d7183bdc89b3561d91b42be
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@netapp.com>
2014-07-16 14:17:05 +0200

    Use this shell script to test testbed

---
+++ measurement/tcp-ancr_eval/new-scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USAGE="Usage: $0 duration log-folder"
+
+if [ $# -lt 2 ]; then
+    echo $USAGE
+    exit 1
+fi
+
+DURATION=$1
+FOLDER=$2
+
+./test-1.py pair.conf -l $FOLDER -o 0 -i 1 -t $DURATION
